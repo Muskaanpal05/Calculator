be23a9a9bd2448155ad0cc687528c736dabb6dd9
Muskaanpal05 <palmuskaan5@gmail.com>
2020-10-16 12:47:46 +0530

    Processing Post Request with Body Parser

--- calculator.js
+++ calculator.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-    //  res.send("index.html");  Realtive path
+    //  res.sendFile("index.html");  Realtive path
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+    var result = n1 + n2;
+    res.send("The result of the calculation is :" + result);
 });
 
 app.listen(3000, function(req, res) {
